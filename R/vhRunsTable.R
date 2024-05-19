96d50b2b1ba4e1c27fc6a47e76c7a47559f10f0c
SergejRuff <serijnh@gmail.com>
2024-05-19 23:24:22 +0200

    add title option to table

--- R/vhRunsTable.R
+++ R/vhRunsTable.R
@@ -1,11 +1,13 @@
-#' @title Generate a gt summary table of unique SRA runs for each virus group
+#' @title Generate a gt summary table of unique runs for each virus group
 #'
-#' @description This function generates a summary table of unique SRA runs for each virus group
+#' @description This function generates a summary table of unique runs for each virus group
 #' based on the input dataset.
 #'
 #' @param vh_file A data frame containing the Virushunter Hittables Results
 #' @param cut The significance cutoff value for E-values (default: 1e-5). Removes rows in vh_file
 #' with values larger than cutoff value in ViralRefSeq_E column.
+#' @param title (optional): a title for the summary title.
+#' Default is "Summary Table of Unique Runs for Each Virus Group"
 #'
 #' @return A formatted gt table summarizing unique runs for each virus group
 #'
@@ -27,7 +29,7 @@
 #' @importFrom gt gt
 #' @importFrom rlang .data
 #' @export
-vhRunsTable <- function(vh_file,cut = 1e-5){
+vhRunsTable <- function(vh_file,cut = 1e-5,title="Summary Table of Unique Runs for Each Virus Group"){
 
   vh_file <- vh_file[vh_file$ViralRefSeq_E < cut,]
 
@@ -40,7 +42,11 @@ vhRunsTable <- function(vh_file,cut = 1e-5){
   which_runs$SRAs_found <- gsub("[c()\\\\\"]", "", which_runs$SRAs_found)
 
   # creat a table
-  which_runs_table <- which_runs %>% gt()
+  which_runs_table <- which_runs %>%
+    gt()%>%
+    tab_header(
+      title = title
+    )
 
   return(which_runs_table)
 
