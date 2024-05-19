38e74110d13017fe4f3bb8c223a3af5ed111f4f2
SergejRuff <serijnh@gmail.com>
2024-05-20 01:18:14 +0200

    add title option to table

--- R/vhRunsTable.R
+++ R/vhRunsTable.R
@@ -17,15 +17,19 @@
 #' @param subtit (optional): a character vector specifying the subtitle. Default is NULL.
 #' @param data_row.pad (optional): numeric value specifying the row padding. Default is 6.
 #' @param column_colour (optional): character specifying the background colour for the column header.
+#' Default is dodgerblue4.
 #' @param title_size (optional): numeric value specifying title size. Default is 26.
 #' @param subtitle_size (optional): numeric value specifying subtitle size. Default is 14.
 #' @param title_weight (optional): character or numeric value specifying title font weight.
 #' The weight of the font can be modified thorough a text-based option such as "normal", "bold",
 #' "lighter", "bolder", or, a numeric value between 1 and 1000, inclusive.Default value is "bold".
-#' @param title_colour (optional): character specifying title colour. Default is NULL.
+#' @param title_colour (optional): character specifying title colour. Default is dodgerblue4.
 #' @param table_font_size  (optional): numeric value specifying table font size. This will change font
 #' size for the column header and for all values in each cell. Default is 14.
-#' @param cell_colour (optional): character specifying cell colour.
+#' @param cell_colour (optional): character specifying cell colour.Default is grey90.
+#' @param col_everyrow (optional): bool value specifying if every row or every second row of the table
+#' should be filled with the colour from the cell_colour argument. col_everyrow = TRUE colours every
+#' row and col_everyrow = FALSE colours every second row. Default is FALSE.
 #'
 #' @return A formatted gt table summarizing unique runs for each virus group
 #'
@@ -57,8 +61,8 @@
 vhRunsTable <- function(vh_file,cut = 1e-5,title="Summary Table of Unique Runs for Each Virus Group",
                         title_align = "left",names_=NULL,align = "left",subtit =NULL,
                         data_row.pad=6,column_colour="dodgerblue4",title_size = 26,subtitle_size=14,
-                        title_weight="bold",title_colour=NULL,table_font_size = 14,
-                        cell_colour="grey90"){
+                        title_weight="bold",title_colour = "dodgerblue4",table_font_size = 14,
+                        cell_colour="grey90",col_everyrow=FALSE){
 
   if(is.null(names_)){
     names_ <- c("Virus Group","Number of Unique SRA Runs","SRAs Found")
@@ -118,11 +122,23 @@ vhRunsTable <- function(vh_file,cut = 1e-5,title="Summary Table of Unique Runs f
       ),
       locations = cells_title(groups = "title")
     ) %>%
-    tab_options(table.font.size = px(table_font_size))%>%
-    tab_style(
-      style = cell_fill(color = cell_colour),
-      locations = cells_body(rows = seq(1,row_number,2))
-    )
+    tab_options(table.font.size = px(table_font_size))
+
+  if(col_everyrow==FALSE){
+    which_runs_table <- which_runs_table%>%
+      tab_style(
+        style = cell_fill(color = cell_colour),
+        locations = cells_body(rows = seq(1,row_number,2))
+      )
+  }
+
+  if(col_everyrow==TRUE){
+    which_runs_table <- which_runs_table%>%
+      tab_style(
+        style = cell_fill(color = cell_colour),
+        locations = cells_body(rows = seq(1,row_number))
+      )
+  }
 
 
   return(which_runs_table)
