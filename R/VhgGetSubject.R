c6e8810a9080bceb3e2922180fa6dcc108fd3a24
SergejRuff <serijnh@gmail.com>
2024-08-04 19:31:39 +0200

    added include sra_id to getsubject

--- R/VhgGetSubject.R
+++ R/VhgGetSubject.R
@@ -4,6 +4,9 @@
 #' @param groupby (optional): A character specifying the column containing the groups (default: "best_query").
 #' Note: Gatherer hittables do not have a "best_query" column. Please provide an appropriate column for grouping.
 #' @param remove_identifiers (optional): if `TRUE` (default), removes the identifiers in the ViralRefSeq_subject cells.
+#' @param include_run_ids (optional): If `TRUE` (default is `TRUE`), adds a fourth column named `run_ids` to the output.
+#' This column contains a comma-separated list of unique identifiers from either the `SRA_run` or `run_id` column,
+#' aggregated for each combination of group and subject.
 #'
 #' @details
 #' The function `VhgGetSubject` counts the number of viral subjects in the `ViralRefSeq_subject` column
@@ -39,7 +42,8 @@
 #' @export
 VhgGetSubject <- function(file,
                           groupby = "best_query",
-                          remove_identifiers = TRUE){
+                          remove_identifiers = TRUE,
+                          include_run_ids = FALSE){
 
   #is_file_empty(file)
   if (is_file_empty(file)) {
@@ -52,19 +56,52 @@ VhgGetSubject <- function(file,
   }
 
   arg_logical(remove_identifiers)
+  arg_logical(include_run_ids)
+
+
+  # Determine the column to use for run_id_values
+  if ("SRA_run" %in% colnames(file)) {
+    run_id_column <- "SRA_run"
+  } else if ("run_id" %in% colnames(file)) {
+    run_id_column <- "run_id"
+  } else {
+    stop('Neither "SRA_run" nor "run_id" column found in the file.')
+  }
 
   # Process the data based on the remove_identifiers argument
-  result <- file %>%
+  file <- file %>%
     mutate(Processed_ViralRefSeq_subject = if (remove_identifiers) {
       str_extract(.data$ViralRefSeq_subject, "(?<=\\|).*")
     } else {
       .data$ViralRefSeq_subject
     })
 
-  # Process and count matching subjects
-  result <- result %>%
+
+
+  # Group by the specified column and the processed viral subjects
+  result <- file %>%
     group_by(across(all_of(groupby)), .data$Processed_ViralRefSeq_subject) %>%
-    summarise(subject_count = n(), .groups = 'drop')
+    summarise(
+      subject_count = n(),
+      .groups = 'drop'
+    )
+
+  # Optionally include run_id_values
+  if (include_run_ids) {
+    run_id_values <- file %>%
+      group_by(across(all_of(groupby)), .data$Processed_ViralRefSeq_subject) %>%
+      summarise(
+        run_ids = paste(unique(.data[[run_id_column]]), collapse = ", "),
+        .groups = 'drop'
+      )
+    result <- result %>%
+      left_join(run_id_values, by = c(groupby, "Processed_ViralRefSeq_subject"))
+  }
+
+
+
+
+
   return(result)
 
 
